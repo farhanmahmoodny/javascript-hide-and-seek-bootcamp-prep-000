b20c7b00c30cb02ae65b423e65a4c52f70110247
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-01 02:48:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  var list = document.getElementById('#app').querySelectorAll('ul.ranked-list li')
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
   for(var i = 0; i <list.length; i++){
     parseInt(list[i].innerHTML) + n
   }
