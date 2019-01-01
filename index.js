8fd375b0372f42544f985ac56e71636db8f6b87d
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-01 01:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,12 +12,13 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  var doc = document
-  .getElementById('app')
-  .querySelectorAll('ul.randked-list li')
-  for(var i = 0; i < doc.length; i++){
-    var increaseBy = n
-    doc[i].innerHTML = (i + increaseBy).parseInt()
-  }
-  return doc
+  // var doc = document
+  // .getElementById('app')
+  // .querySelectorAll('ul.randked-list li')
+  // for(var i = 0; i < doc.length; i++){
+  //   var increaseBy = n
+  //   doc[i].innerHTML = (i + increaseBy).parseInt()
+  // }
+  // return doc
+  return parseInt(document.getElementById('app').querySelectorAll('ul.ranked-list li') + n
 }
