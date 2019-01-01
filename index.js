81cae342412446fa6629a57bb2439e45a05bba40
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-01 03:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function increaseRankBy(n){
   .getElementById('app')
   .querySelectorAll('ul.ranked-list li')
   for(var i = 0; i <list.length; i++){
-    parseInt(list[i].innerHTML)
-    list[i].innerHTML += parseInt(n)
+    parseInt(list[i].innerHTML) + n
   }
   console.log(list)
 }
