b10d2977365f26d157d60866a0c7c7bfe1b0b467
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-01 03:10:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#app #nested .target')
+}
+
+function deepestChild(){
+  return document.querySelector('#app #grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  var list = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li')
+  for(var i = 0; i <list.length; i++){
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n
+  }
+}
