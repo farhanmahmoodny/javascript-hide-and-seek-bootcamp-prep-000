5d62d1ef9d3f8c4ff35045823530b6a5e7138275
Farhan Mahmood <farhanmahmoodny@gmail.com>
2019-01-01 01:32:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function nestedTarget(){
   var app = document.getElementById('app')
   var nested = app.div
   // var className = 'target'
-  var target = nested.getElementByClass('target')
+  var target = nested.div[3]
   return target
 }
